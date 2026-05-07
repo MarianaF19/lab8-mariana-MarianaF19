8132a1e41420b978d5c5a0b503022790c86b8374
Mariana F <marianafama@unisabana.edu.co>
2026-05-07 13:53:39 -0500

    8

--- question1.py
+++ question1.py
@@ -4,3 +4,16 @@ Implementa una CLI que calcule carga por punto de soporte.
 """
 
 # TODO: Implementar según README.md
+import sys
+ 
+def main():
+    try:
+        if len(sys.argv) < 3:
+            raise ValueError
+ 
+        total_load = float(sys.argv[1])
+        num_supports = float(sys.argv[2])
+ 
+        if num_supports == 0:
+            print("Error: Cannot divide by zero! Supports must be greater than zero.")
+            return
