bf6265f48bb9607e7e98d4e49aed959b8abf088a
Mariana F <marianafama@unisabana.edu.co>
2026-05-07 13:25:02 -0500

    1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 """Laboratorio 8 - CLI del gestor de tareas."""
 
-# TODO: Implementar CLI según README.md
+# TODO:Implementar CLI según README.md
+import sys
+from todo_manager import read_todo_file, write_todo_file
+ 
+def main():
+ 
+    if len(sys.argv) < 2:
+        print("Insufficient arguments provided!")
+        return
